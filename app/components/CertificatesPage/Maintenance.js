c7b4b1afd63d378616ff7f547616314dae0f3ae2
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2023-05-15 14:36:31 +0300

    added videos

--- app/components/CertificatesPage/Maintenance.js
+++ app/components/CertificatesPage/Maintenance.js
@@ -1,16 +1,23 @@
 import { s } from "@/app/styles"
 
 export const Maintenance = () => {
-    return <section className={`${s.container} pt-36 pb-20 md:pt-[14rem] relative ${s.bgImage} !bg-cover !bg-bottom bg-[url('./images/bg_certificates_plane_mob.png')] md:bg-[url('./images/bg_certificates_plane.png')]`}>
+    return <section className={`${s.container} pt-36 pb-20 md:pt-[14rem] relative bg-gradient-to-b from-black via-[#222] to-black`}>
+        <div className="absolute top-0 left-0 grid place-items-center h-full w-auto overflow-hidden mix-blend-lighten">
+            <div className="w-[508%] md:w-[110%]">
+                <video autoPlay loop muted className="h-full w-auto opacity-[12%]">
+                    <source src={"/heavy1.mp4"} type="video/mp4" />
+                </video>
+            </div>
+        </div>
         <div className="absolute top-[-3rem] md:top-[-16rem] h-32 md:h-[28rem] w-full bg-[url('./images/line_1_mob.svg')] md:bg-[url('./images/line_1.svg')] bg-center bg-cover bg-no-repeat mx-auto" />
-        <div>
+        <div className="z-10">
             <p className={`${s.text} max-w-[77.8125rem] mb-20`}>
                 We&apos;re proud to hold an EASA 145 certificate approved for Base and Line maintenance, which is a testament to our commitment to providing high-quality maintenance, repair, and overhaul services for aircraft. The EASA 145 certification is a comprehensive standard that covers all aspects of aircraft maintenance and ensures that our operations are conducted safely and efficiently. To obtain and maintain our EASA 145 certification, we undergo regular audits by the European Aviation Safety Agency (EASA), which is responsible for regulating civil aviation safety in Europe. This certification demonstrates that we have met and exceeded the strict standards set by EASA for aircraft maintenance and repair. To ensure that we deliver the best possible service, we invest in the ongoing education and certification of our team of expert technicians and engineers. We work with industry-leading organizations to obtain certifications that demonstrate our commitment to excellence, safety, and environmental responsibility.
                 <br /><br />
                 Our certifications are more than just pieces of paper - they represent our dedication to providing exceptional service to our customers. When you choose Heavytech MRO you can trust that you&apos;re working with a team that is highly trained and certified in the latest industry standards and best practices. We&apos;re also proud to hold both PART 21G and J approvals, which allow us to design, manufacture, and certify aircraft components and modifications. These certifications are a testament to our commitment to providing high-quality MRO services and ensuring the airworthiness of the aircraft we work on. The PART 21G certification allows us to design and manufacture aircraft parts and components, while the J approval enables us to modify and certify these components for installation on aircraft. Together, these certifications enable us to offer a wide range of customized solutions for our customers, from minor modifications to complete overhauls.
             </p>
             <div className="hidden md:block absolute left-0 h-[5rem] w-full bg-[url('./images/line_2.svg')] bg-center bg-contain bg-no-repeat" />
-            <div className="h-[14.375rem] w-auto bg-[url('./images/logo_big.svg')] bg-center bg-contain bg-no-repeat md:mt-[21.5rem] md:mb-[16.5rem]" />
+            <div className="h-[14.375rem] w-auto bg-[url('./images/logo_big.svg')] bg-center bg-contain bg-no-repeat md:mt-[21rem] md:mb-[16rem]" />
             <div className="absolute left-0 bottom-0 h-[5rem] w-full bg-[url('./images/line_2_mob.svg')] md:bg-[url('./images/line_2.svg')] bg-center bg-contain bg-no-repeat md:rotate-180" />
         </div>
     </section>
--- app/components/HomePage/Landing.js
+++ app/components/HomePage/Landing.js
@@ -2,7 +2,14 @@ import { s } from "@/app/styles"
 import { Buttons } from "../Buttons"
 
 export const Landing = () => {
-    return <section className={`${s.container} ${s.bgImage} pt-80 md:pt-[36rem] pb-9 !bg-bottom !bg-cover`}>
+    return <section className={`${s.container} pt-80 md:pt-[36rem] pb-9 bg-gradient-to-b from-[#424242] to-[#111] relative`}>
+        <div className="absolute top-0 left-0 grid place-items-center h-full w-auto overflow-hidden mix-blend-multiply">
+            <div className="max-sm:w-[320%] h-full">
+                <video autoPlay loop muted className="h-full w-auto opacity-75">
+                    <source src={"/heavy.mp4"} type="video/mp4" />
+                </video>
+            </div>
+        </div>
         <div className="grid grid-flow-row place-items-center z-10">
             <div className="mb-14 md:mb-5">
                 <p className={`${s.mainSubtitle} mb-6 md:mb-1`}>Reliable and trust worthy partner for your business</p>
